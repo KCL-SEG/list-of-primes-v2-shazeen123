1b235bd051b38f042177c97ed23256dcf1d09283
shazeen123 <shazeenshaheen2003@gmail.com>
2022-12-03 14:54:56 +0000

    solve

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
-"""List of prime numbers generator."""
-"""ENTER YOUR SOLUTION HERE!"""
-
 def primes(number_of_primes):
+
     list = []
+    n = 2
+
+    if (number_of_primes <= 0):
+        raise ValueError(f"numberOfPrimes = {number_of_primes} should be positive.")
+
+    else:
+        while (len(list) < number_of_primes):
+            for i in range(2, n):
+                if n % i == 0:
+                    n += 1
+                    break
+            else:
+                list.append(n)
+                n += 1
+                
+    print(list)
     return list
